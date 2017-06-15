acba0d429997bc7e1c0ffae69a1c1e40885368e4
Nathan Southerland <nates1973@yahoo.com>
2017-06-15 12:05:33 -0500

    Adding stub projects for bot, web api, and supporting domain models and data access layer

---
+++ src/WeirdBot/WeirdBot.DataAccess/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeirdBot.DataAccess.Models
+{
+    public class ProductRepository
+    {
+    }
+}
