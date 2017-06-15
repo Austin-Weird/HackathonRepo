9cd229ddd1b4aeb5ea46d97e061c8fa646457094
John Denton <dragonfyregold@gmail.com>
2017-06-15 18:01:05 -0500

    Initial Bot Framework Project in DotNet Core.  It has some isuses and errors trying to get the data message back, but otherwise a buildable working base. - Work in progress.

---
+++ BotApi/ServiceModels/BotCredentials.cs
@@ -0,0 +1,18 @@
+﻿namespace Bot.Api.ServiceModels
+{
+    /// <summary>
+    /// Bot Credentials
+    /// </summary>
+    public class BotCredentials
+    {
+        /// <summary>
+        /// CLientId
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// ClientSecret
+        /// </summary>
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ BotApi/Startup.cs
@@ -0,0 +1,92 @@
+﻿using Bot.Api.ServiceModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Bot.Api
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+        public IConfigurationRoot Configuration { get; }
+        private const string CORS_POLICY_NAME = "AllowSpecificOrigin";
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_env.IsProduction() || _env.IsStaging())
+            {
+                // Implement a real Services
+            }
+            else
+            {
+                // Implement a fake Services
+                //services.AddScoped<IFakeService, DebugFakeService>();
+            }
+            services.AddLogging();
+            services.AddSingleton(Configuration);
+
+            services.Configure<BotCredentials>(Configuration.GetSection("BotCredentials"));
+
+            // We need to cache tokens, so enable MemoryCache.
+            services.AddMemoryCache();
+
+            // Add framework services.
+            services.AddMvcCore()
+                        .AddJsonFormatters()
+                        //.AddCors(options =>
+                        //{
+                        //    options.AddPolicy(CORS_POLICY_NAME, builder =>
+                        //    {
+                        //        builder
+                        //            .WithOrigins("http://localhost:4000")
+                        //            //.AllowAnyOrigin()
+                        //            .AllowAnyHeader()
+                        //            .AllowAnyMethod()
+                        //            //.AllowCredentials()
+                        //            .Build();
+                        //    });
+                        //})
+            ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {   // ORDER MATTERS IN HERE!
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug(Configuration.GetSection("Logging").GetValue<LogLevel>("LogLevel"));
+
+            ///* Enabling swagger file */
+            //app.UseSwagger();
+
+            ///* Enabling Swagger ui, consider doing it on Development env only */
+            //app.UseSwaggerUi();
+
+            //app.UseCors(CORS_POLICY_NAME);
+            app.UseMvc(
+                //config =>
+                //{
+                //    config.MapRoute(
+                //        name: "Default",
+                //        template: "api/{controller}/{action}/{id?}"//,
+                //        //defaults: new { controller = "Home", action = "Index" }
+                //    );
+                //}
+            );
+        }
+    }
+}
